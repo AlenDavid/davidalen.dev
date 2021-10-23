7f66b8d80799559cb223bfed72ed22552268c288
David Alen <david.alen@fabric.inc>
2021-10-23 18:18:48 -0300

    ok it's formatting now

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,15 @@
+import React from 'react'
+
 export default function Home() {
-  return (
-    <div>
-      <h1>David Alen!</h1>
-    </div>
-  )
+	return (
+		<main>
+			<div className='bg-black text-white h-4/6 w-screen'>
+				<h1 className='text-4xl'>David Alen!</h1>
+				<p className='text-lg'>Senior Software Developer</p>
+			</div>
+			<div>
+				<h2>Presentation</h2>
+			</div>
+		</main>
+	)
 }
