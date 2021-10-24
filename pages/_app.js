9e32e56fe535e62e4ab6cd940f00497f5976a700
David Alen <david.alen@fabric.inc>
2021-10-24 02:35:41 -0300

    (fix) viewport fixed

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,24 @@
 import React from 'react'
+import Head from 'next/head'
 
 import '../styles/globals.css'
+import 'tailwindcss/tailwind.css'
 
 // eslint-disable-next-line react/prop-types
 function MyApp({Component, pageProps}) {
-	return <Component {...pageProps} />
+	return (
+		<>
+			<Head>
+				<title property='og:title'>David Alen</title>
+				<meta
+					name='viewport'
+					content='minimum-scale=1, initial-scale=1, width=device-width'
+					key='viewport-meta'
+				/>
+			</Head>
+			<Component {...pageProps} />
+		</>
+	)
 }
 
 export default MyApp
